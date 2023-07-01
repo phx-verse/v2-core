df0da3789775c4bdfb3c876c4ce3c8a04945ac2e
phx9 <phx9@pm.me>
2023-07-01 16:53:44 +0800

    update deploy script

--- deploy/03_deploy_phx_rate.js
+++ deploy/03_deploy_phx_rate.js
@@ -4,14 +4,19 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
     const PHXRate = await ethers.getContractFactory("PHXRate");
+    const startTime = parseInt(Date.UTC(2023, 6, 3, 8, 0, 0) / 1000); // 2023.7.3 08:00 (UTC+0)
     const txData = PHXRate.interface.encodeFunctionData("initialize", [
         [
             {
-                startTime: parseInt(Date.now() / 1000) + 60 * 60 * 1,
-                rate: 115740740740740740n, // one year 365w phx
+                startTime: parseInt(Date.now() / 1000) + 60 * 5,
+                rate: 0n,
             },
             {
-                startTime: parseInt(Date.now() / 1000) + 60 * 60 * 24 * 365,
+                startTime: startTime,
+                rate: 190258751902587519n, // 4 year 2400w phx
+            },
+            {
+                startTime: startTime + 60 * 60 * 24 * 365 * 4,
                 rate: 0n,
             },
         ],
--- script/hardhat/eSpace/02_transferPHX.js
+++ script/hardhat/eSpace/02_transferPHX.js
@@ -5,11 +5,11 @@ main().catch(console.log);
 async function main() {
     const contract = await ethers.getContractAt(
         "PHXTreasury",
-        process.env.FUND_TREASURY
+        process.env.TREASURY
     );
     const tx = await contract.transfer(
         process.env.TARGET,
-        ethers.utils.parseEther("1000000")
+        ethers.utils.parseEther("24000000")
     );
     await tx.wait();
     console.log("PHX transferred from treasury");
